c0bfac163a463e2f2b5b9172016526401d91cac4
Sardorbek Khusenov <sardorkhusenov@gmail.com>
2026-06-19 16:27:10 +0500

    feat(user): add delete-user use case

---
+++ src/application/user/delete-user.ts
@@ -0,0 +1,27 @@
+import type { UserRepository } from '@/domain/user/user-repository';
+import { UserNotFoundError } from '@/domain/user/user-errors';
+
+export interface DeleteUserInput {
+  id: string;
+}
+
+export type DeleteUserOutput = void;
+
+interface DeleteUserDeps {
+  userRepository: UserRepository;
+}
+
+export class DeleteUser {
+  private readonly users: UserRepository;
+
+  constructor({ userRepository }: DeleteUserDeps) {
+    this.users = userRepository;
+  }
+
+  async execute(input: DeleteUserInput): Promise<DeleteUserOutput> {
+    const user = await this.users.findById(input.id);
+    if (!user) throw new UserNotFoundError(input.id);
+    user.softDelete();
+    await this.users.update(user);
+  }
+}
