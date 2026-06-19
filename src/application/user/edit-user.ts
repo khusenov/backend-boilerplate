5f3e0fd1ed13c7c6d4c91cf5533e065fba50a189
Sardorbek Khusenov <sardorkhusenov@gmail.com>
2026-06-19 16:16:24 +0500

    feat(user): add edit-user use case
    Support partial updates of name and email, guarding email
    changes with a uniqueness check before persisting.

---
+++ src/application/user/edit-user.ts
@@ -0,0 +1,45 @@
+import { toUserDto, type UserDto } from '@/application/user/user-dto';
+import type { UserRepository } from '@/domain/user/user-repository';
+import { EmailAlreadyTakenError, UserNotFoundError } from '@/domain/user/user-errors';
+import { Email } from '@/domain/user/email-vo';
+
+export interface EditUserInput {
+  id: string;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+}
+
+export type EditUserOutput = UserDto;
+
+interface EditUserDeps {
+  userRepository: UserRepository;
+}
+
+export class EditUser {
+  private readonly users: UserRepository;
+
+  constructor({ userRepository }: EditUserDeps) {
+    this.users = userRepository;
+  }
+
+  async execute(input: EditUserInput): Promise<EditUserOutput> {
+    const user = await this.users.findById(input.id);
+    if (!user) throw new UserNotFoundError(input.id);
+
+    if (input.email !== undefined) {
+      const email = Email.create(input.email);
+      if (!user.email.equals(email)) {
+        const existingUser = await this.users.findByEmail(email);
+        if (existingUser) throw new EmailAlreadyTakenError(email.toString());
+        user.changeEmail(email);
+      }
+    }
+
+    if (input.firstName !== undefined) user.changeFirstName(input.firstName);
+    if (input.lastName !== undefined) user.changeLastName(input.lastName);
+
+    await this.users.update(user);
+    return toUserDto(user);
+  }
+}
